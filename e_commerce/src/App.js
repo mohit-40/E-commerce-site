b20ce54a4de6053156956b34d684140db1290379
Mohit agrawal <2019285@iiitdmj.ac.in>
2021-10-02 06:24:35 +0530

    make navbar

--- e_commerce/src/App.js
+++ e_commerce/src/App.js
@@ -12,7 +12,6 @@ function App() {
       <Router>
         <Switch>
           <Route path="/"> <Home /> </Route>
-          
         </Switch>
       </Router> 
   );
--- e_commerce/src/Component/Navbar/Navbar.jsx
+++ e_commerce/src/Component/Navbar/Navbar.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import styled from 'styled-components'
+
+const Container=styled.div``
+const Wrapper=styled.div``
+const Language=styled.div``
+const SearchBar=styled.div``
+const Left=styled.div``
+const Center=styled.div``
+const Right=styled.div``
+const RightItem=styled.div``
 
 const Navbar = () => {
 	return (
-		<div>
-			
-		</div>
+		<Container>
+			<Wrapper>
+				<Left>
+					<Language>EN</Language>
+					<SearchBar>
+						<input type="text" />
+						<Search/>
+					</SearchBar>
+				</Left>
+				<Center>LAMA DEV</Center>
+				<Right>
+					<RightItem>SignUp</RightItem>
+					<RightItem>SignIN</RightItem>
+					<RightItem> <ShoppingCartOutlined /> </RightItem>
+				</Right>
+			</Wrapper>
+		</Container>
 	)
 }
 
