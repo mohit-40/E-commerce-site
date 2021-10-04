dfc68967d6bf72761110fb7cb847f723963194ad
Mohit agrawal <2019285@iiitdmj.ac.in>
2021-10-04 13:18:35 +0530

    remove unwanted file and install pakages

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <>
+      <Router>
+        <Switch>
+          <Route exact path="/"></Route>
+        </Switch>
+      </Router>
     </>
   );
 }
