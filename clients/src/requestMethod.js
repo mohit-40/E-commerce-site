d3fd2f3ba34de750ce71fc5fe293de2c5cf55e56
Mohit agrawal <2019285@iiitdmj.ac.in>
2022-01-12 01:01:57 +0530

    .

--- clients/src/requestMethod.js
+++ clients/src/requestMethod.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-const BASE_URL = "./";
+const BASE_URL = "https://shophify.herokuapp.com/shopping website/";
 
 export const publicRequest = axios.create({ baseURL: BASE_URL })
 export const userRequest =axios.create({ baseURL: BASE_URL })
