d0b179df33f96588233c74d7974858b10711d10e
Mohit agrawal <2019285@iiitdmj.ac.in>
2022-01-12 13:16:28 +0530

    .

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ mongoose.connect(process.env.MONGO_URL).then(()=>console.log("connected to mongo
 app.use(express.json())
 app.use(helmet());
 app.use(cors());
+
 app.use(
 	helmet.contentSecurityPolicy({
 	  useDefaults: true,
