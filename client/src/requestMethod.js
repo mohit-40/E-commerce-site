6a1ea419d44d8d7e6417a72dea042b0514e0530e
Mohit agrawal <2019285@iiitdmj.ac.in>
2022-01-13 18:56:59 +0530

    dev friendly

--- client/src/requestMethod.js
+++ client/src/requestMethod.js
@@ -2,8 +2,10 @@
 
 import axios from "axios";
 
-const BASE_URL = "https://shophify.herokuapp.com/api";
+let BASE_URL = "http://localhost:8800/api";
+if (process.env.NODE_ENV === "production") {
+	BASE_URL = "https://shophify.herokuapp.com/api";
+} 
 
 export const publicRequest = axios.create({ baseURL: BASE_URL })
 export const userRequest =axios.create({ baseURL: BASE_URL })
-
