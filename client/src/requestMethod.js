891c950924b1de1111b1a0deb9a20a1b8241d9be
Mohit agarawal <2019285@iiitdmj.ac.in>
2023-05-09 18:21:23 +0530

    change base url

--- client/src/requestMethod.js
+++ client/src/requestMethod.js
@@ -4,8 +4,7 @@ import axios from "axios";
 
 let BASE_URL = "http://localhost:8800/api";
 if (process.env.NODE_ENV === "production") {
-	BASE_URL = "https://shophify.herokuapp.com/api";
+	BASE_URL = "https://fashionstore.cyclic.app/api";
 } 
-
 export const publicRequest = axios.create({ baseURL: BASE_URL })
 export const userRequest =axios.create({ baseURL: BASE_URL })
