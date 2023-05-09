2ff67e71fd4773d50da52d002e1043fd5494a92b
Mohit agarawal <2019285@iiitdmj.ac.in>
2023-05-09 18:50:18 +0530

    added the build folder

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.use("/api/email",emailRoute);
 if (process.env.NODE_ENV === "production") {
 	console.log("in production")
 	const path = require("path")
-	app.use(express.static(path.join(__dirname, "/client/build")));
+	app.use(express.static(path.join(__dirname, "./client/build")));
 	
 	app.get('*', (req, res) => {
 		res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
